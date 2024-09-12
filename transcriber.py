a51e39d63d72374ededa211bbcf7064db569f00a
Max Siebengartner <106451891+MaxRS07@users.noreply.github.com>
2024-09-12 23:46:48 +0800

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -4,9 +4,9 @@ import tempfile
 import io
 from utils import save_temp, encode_image
 
-DUBDUB = "5244d18eaf3a4eb3a7b150ff3615c435"
-KEY = "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"
-KEY2 = "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"
+DUBDUB = "KEY"
+KEY = "APIKEY"
+KEY2 = "APIKEY"
 def generate(audio=AudioFileClip):
     model_size = "small"
 
